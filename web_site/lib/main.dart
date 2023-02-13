bd129541b362caaaeb67b4e7497845e8d968e2df
samlevy25 <samuelevy25@gmail.com>
2023-02-13 14:31:35 +0200

    add dependencies.

--- web_site/lib/main.dart
+++ web_site/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp();
 }
 
 class MainApp extends StatelessWidget {
